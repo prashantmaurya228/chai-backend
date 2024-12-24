4854bfd0337bd6dde553d3df76a33de9e90b0919
prashantmaurya228 <34316534+prashantmaurya228@users.noreply.github.com>
2024-12-24 11:02:49 +0000

    untils added

--- src/db/index.js
+++ src/db/index.js
@@ -3,10 +3,10 @@
  const connectDB = async () => {
     try {
         const connectionInstance = await mongoose.connect("mongodb://localhost:27017/testDB")
-        console.log("connection sucssesfull")
+        console.log("connection sucssesfull done")
     }
     catch(error){
-        console.log("DB CONNECTION FAILED",error)
+        console.log("DB CONNECTION FAILED",error) 
     }
  }
 
--- src/index.js
+++ src/index.js
@@ -2,11 +2,18 @@
 import dotenv from "dotenv"
 import mongoose from "mongoose";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
     path:'./env',
-    
+
 })
 
-
-connectDB();
+connectDB().then(()=>{
+    app.listen(process.env.PORT||8000,()=>{
+        console.log(`server is running on port ${process.env.PORT}`)
+    })
+}).catch((err)=>{
+    console.log(err);
+    
+});
